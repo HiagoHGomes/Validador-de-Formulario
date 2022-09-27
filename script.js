e898089d990e4b3465d0f70808e341948e31db6e
Hiago Henrique <hiago_h3@hotmail.com>
2022-09-27 20:26:59 -0300

    Concluído

--- script.js
+++ script.js
@@ -1,41 +1,79 @@
 let validador = {
     handleSubmit:(event)=> {
         event.preventDefault();
-        let sand = true;
+        let send = true;
         let inputs = form.querySelectorAll('input');
+
+        validador.clearerrors();
 
         for(let i=0; i<inputs.length; i++) {
             let input = inputs[i];
             let check = validador.cehckInput(input);
             if (check!==true) {
-                sand = false;
+                send = false;
                 //Eixibir erro:
-                console.log(check)
+                validador.showError(input, check);
             }
         }
 
 
-        if(sand) {
+        if(send) {
             form.submit();
         }
     },
 
     cehckInput:(input)=> {
         let rules = input.getAttribute('data-rules');
-        if (rules !=null) {
+
+        if(rules !== null) {
             rules = rules.split('|');
             for(let k in rules) {
-                let rDetalis = rules[k].split('=');
-                switch(rDetalis[0]) {
+                let rDetails = rules[k].split('=');
+                switch(rDetails[0]) {
                     case 'required':
                         if(input.value == '') {
-                            return 'It can not be blank.'
+                            return 'Campo não pode ser vazio.';
+                        }
+                    break;
+                    case 'min':
+                        if(input.value.length < rDetails[1]) {
+                            return 'Campo tem que ter pelo menos '+rDetails[1]+' caractes';
+                        }
+                    break;
+                    case 'email':
+                        if(input.value != '') {
+                            let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                            if(!regex.test(input.value.toLowerCase())) {
+                                return 'E-mail digitado não é válido!';
+                            }
                         }
                     break;
                 }
             }
-        }else {
-            return true;
+        }
+
+        return true;
+    },
+
+    showError:(input, error)=> {
+        input.style.borderColor = '#FF0000';
+
+        let errorElement = document.createElement('div') // Para criar uma tag
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+
+        input.parentElement.insertBefore(errorElement, input.ElementSibling); //Posição onde a tag irá ficar.
+    },
+
+    clearerrors:()=> {
+        let inputs = form.querySelectorAll('input');
+        for(let i=0;i<inputs.length;i++) {
+            inputs[i].style = '';
+        }
+
+        let errorElements = document.querySelectorAll('.error');
+        for(let i=0;i<errorElements.length;i++) {
+            errorElements[i].remove();
         }
     }
 
